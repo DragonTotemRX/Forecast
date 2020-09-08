18aea4a58bed2aa2854debe5c51b9d05ce4bb130
DragonTotemRX <42271639+DragonTotemRX@users.noreply.github.com>
2020-09-08 16:08:36 -0500

    Add files via upload

---
+++ ngf_step1_CB.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import psutil
+import time
+
+max_mem=70
+
+print('NGF Nvidia Training and Predicting')
+print('Step 1) Run train and predict - BEGIN')
+print('Arguments are [fog] and [period]'+str(sys.argv))
+
+my_code_loc = '/rapids/notebooks/my_code/'
+
+while psutil.virtual_memory().percent>max_mem: 
+    print('total ps='+str(len(psutil.pids())))
+    print('mem pct='+str(psutil.virtual_memory().percent))
+    time.sleep(60)
+os.system('{ python '+my_code_loc+'CB.py '+sys.argv[1]+' '+sys.argv[2]+' M1_P ; } > '+my_code_loc+'log/'+sys.argv[1]+'_cb_stocked.log 2>&1 &')
+print('clock='+time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime()))
+time.sleep(120)
+
+while psutil.virtual_memory().percent>max_mem: 
+    print('total ps='+str(len(psutil.pids())))
+    print('mem pct='+str(psutil.virtual_memory().percent))
+    time.sleep(60)
+os.system('{ python '+my_code_loc+'CB.py '+sys.argv[1]+' '+sys.argv[2]+' M2_P ; } > '+my_code_loc+'log/'+sys.argv[1]+'_cb_full.log 2>&1 &')
+print('clock='+time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime()))
+
+print('NGF Step 1 Kicked Off')
